0ceff6856cce14039f2d3aee5af52522a1cc5a70
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-08-18 12:36:23 -0300

    properties para opção de tratamento

--- src/services/dados/documento.py
+++ src/services/dados/documento.py
@@ -10,12 +10,13 @@ from src.services.dados.arquivo import Arquivo
 
 class Documento(Arquivo[docx.document.Document]):
 
-    def __init__(self, nome_arquivo: str = None, texto: str = None) -> None:
+    def __init__(self, nome_arquivo: str = None, texto: str = None, opcao_tratamento: int = None) -> None:
         super().__init__(nome_arquivo, texto)
         self.__fonte = 'Ubuntu'
         self.__cor = RGBColor(0, 0, 0)
         self.__alinhamento_justificado = WD_ALIGN_PARAGRAPH.JUSTIFY
         self.__documento = self._abrir_arquivo()
+        self.__opcao_tratamento = opcao_tratamento
 
     def ler_valores(self) -> Generator[Iterable[str], None, None]:
         return super().ler_valores()
@@ -24,31 +25,41 @@ class Documento(Arquivo[docx.document.Document]):
         documento = Document()
         return documento
 
-    def gravar_dados(self, **kwargs):
-        linhas = self.texto.split("\n")
-        for linha in linhas:
-            if linha.startswith("##  "):
-                titulo = self.__documento.add_heading(linha[3:], level=1)
-                for run in titulo.runs:
-                    run.font.color.rgb = self.__cor
-                    run.font.name = self.__fonte
-            elif linha.startswith("### "):
-                subtitulo = self.__documento.add_heading(linha[4:], level=2)
-                for run in subtitulo.runs:
-                    run.font.color.rgb = RGBColor(0, 0, 0)
-                    run.font.name = 'Ubuntu'
-            elif linha.startswith("* "):
+    @property
+    def opcao_tratamento(self):
+        return self.__opcao_tratamento
 
-                paragrafo = self.__documento.add_paragraph(
-                    linha[2:], style='List Bullet')
-                paragrafo.alignment = self.__alinhamento_justificado
-                for run in paragrafo.runs:
-                    run.font.name = self.__fonte
-            elif linha.strip() != "":
-                paragrafo = self.__documento.add_paragraph(linha)
-                paragrafo.alignment = self.__alinhamento_justificado
-                for run in paragrafo.runs:
-                    run.font.name = self.__fonte
+    @opcao_tratamento.setter
+    def opcao_tratamento(self, opcao_tratamento: int):
+        self.__opcao_tratamento = opcao_tratamento
+
+    def gravar_dados(self, **kwargs):
+        if self.__opcao_tratamento == 1:
+            linhas = self.texto.split("\n")
+            for linha in linhas:
+                if linha.startswith("##  "):
+                    titulo = self.__documento.add_heading(linha[3:], level=1)
+                    for run in titulo.runs:
+                        run.font.color.rgb = self.__cor
+                        run.font.name = self.__fonte
+                elif linha.startswith("### "):
+                    subtitulo = self.__documento.add_heading(
+                        linha[4:], level=2)
+                    for run in subtitulo.runs:
+                        run.font.color.rgb = RGBColor(0, 0, 0)
+                        run.font.name = 'Ubuntu'
+                elif linha.startswith("* "):
+
+                    paragrafo = self.__documento.add_paragraph(
+                        linha[2:], style='List Bullet')
+                    paragrafo.alignment = self.__alinhamento_justificado
+                    for run in paragrafo.runs:
+                        run.font.name = self.__fonte
+                elif linha.strip() != "":
+                    paragrafo = self.__documento.add_paragraph(linha)
+                    paragrafo.alignment = self.__alinhamento_justificado
+                    for run in paragrafo.runs:
+                        run.font.name = self.__fonte
 
     def salvar_dados(self, nome_arquivo: str = None):
         caminho_arquivo_salvar = os.path.join(
--- transcricao_video_pipeline.py
+++ transcricao_video_pipeline.py
@@ -24,17 +24,21 @@ class TranscricaoVideoPipeline:
     def __realizar_operacoes_documento_texto_tratado(self, valor: Tuple[str, str], texto_tratado: str):
 
         documento_tratado = self.__documento
+        documento_tratado.opcao_tratamento = 1
         documento_tratado.texto = texto_tratado
         documento_tratado.gravar_dados()
         documento_tratado.salvar_dados(
             nome_arquivo=f'resumo_do_video_{str(valor[1])}.docx')
+        del documento_tratado
 
     def __realizar_operacoes_texto_bruto(self, valor: Tuple[str, str], texto_bruto: str):
         documento_bruto = self.__documento
         documento_bruto.texto = texto_bruto
+        documento_bruto.opcao_tratamento = 2
         documento_bruto.gravar_dados()
         documento_bruto.salvar_dados(
             nome_arquivo=f'transcricao_bruta_{valor[1]}.docx')
+        del documento_bruto
 
     def rodar_pipeline(self):
         for chave, valor in enumerate(self.__arquivo.ler_valores()):
@@ -44,7 +48,7 @@ class TranscricaoVideoPipeline:
             texto_tratado, texto_bruto = self.__servico_chat.obter_resposta_modelo(
                 sentenca=sentenca)
             self.__realizar_operacoes_documento_texto_tratado(
-                valor=valor, setenca=sentenca, texto_tratado=texto_tratado)
+                valor=valor,  texto_tratado=texto_tratado)
             self.__realizar_operacoes_texto_bruto(
                 valor=valor, texto_bruto=texto_bruto)
             self.__arquivo.salvar_dados(linha=chave)
